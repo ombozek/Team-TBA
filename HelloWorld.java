602192238ebdf06632347e70a3ef729c73987ff8
ombozek <ombozek@gmail.com>
2014-09-29 13:20:41 -0700

    privatizing string

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 
-	public final String HELLO_WORLD = "Hello World";
+	private final String HELLO_WORLD = "Hello World";
 	public static void main(String[] args) {
 		System.out.println(HELLO_WORLD);
 	}
