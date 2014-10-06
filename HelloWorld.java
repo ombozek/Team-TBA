66dd54e89fbf2db1687bf19055c2f118978f48e3
ombozek <ombozek@gmail.com>
2014-10-06 16:37:43 -0700

    Adding project layout and basic visual tools

--- HelloWorld.java
+++
@@ -1,7 +0,0 @@
-public class HelloWorld {
-
-	private final String HELLO_WORLD = "Hello World";
-	public static void main(String[] args) {
-		System.out.println(HELLO_WORLD);
-	}
-}
---
+++ TBA/src/GalacticTBA/Saturn.java
@@ -0,0 +1,16 @@
+package GalacticTBA;
+import java.awt.*;
+import java.applet.*;
+
+public class Saturn extends HeavenlyBody {
+
+     public Saturn(HeavenlyBody p, int d, double r, double s, Color c) {
+        super(p, d, r, s, c);
+     }
+
+     public void draw(Graphics g) {
+        super.draw(g);
+        g.fillOval(x + (int) (-0.5*diameter), y + (int) (+0.35*diameter),
+                   2*diameter, (int) (0.3*diameter) );
+    }
+}
